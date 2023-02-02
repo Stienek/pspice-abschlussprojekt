a39085c78b98504e76d8bd45f32e128360a68b88
Stienek <laurin.weitzel@gmail.com>
2023-02-02 17:12:13 +0100

    nvm lul

--- schaltungen-lt/plotting.py
+++ schaltungen-lt/plotting.py
@@ -13,12 +13,18 @@ import os
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-raw1 = RawRead('rohdaten/a11.raw')
-a1vout = raw1.get_trace('V(out)').get_wave()
-a1vin = raw1.get_trace('V(in)').get_wave()
-a1i = raw1.get_trace('I(R1)').get_wave()
-a1rvar = raw1.get_time_axis()
-a1pheiz = a1i*a1vout
+raw11 = RawRead('rohdaten/a11.raw')
+a11vout = raw11.get_trace('V(Vout)').get_wave()
+a11vin = raw11.get_trace('V(Vin)').get_wave()
+a11i = raw11.get_trace('I(R1)').get_wave()
+a11rvar = raw11.get_time_axis()
+a11pheiz = a11i*a11vout
+a11pverlust = a11i*(a11vin - a11vout)
+a11n = 100*a11pheiz/(a11pheiz + a11pverlust)
+
+raw21 = RawRead('rohdaten/a21.raw')
+
+
 
 locale.setlocale(locale.LC_NUMERIC, '')
 plt.rcParams.update({'font.size': 12})
